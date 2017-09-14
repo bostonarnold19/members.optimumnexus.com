e21fb40ed9964cb7fb79d46c8f25a0e0d65c6443
Arnold Boston <aboston@koodi.ph>
2017-09-14 18:25:40 +0800

    data seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Modules\User\Entities\Role;
 use Modules\User\Entities\User;
 
 class UsersTableSeeder extends Seeder
@@ -12,11 +13,14 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        $role_admin = Role::where('name', 'Admin')->first();
+
         $admin = new User();
         $admin->first_name = 'John';
         $admin->last_name = 'Doe';
         $admin->email = 'admin@admin.com';
         $admin->password = bcrypt('admin');
         $admin->save();
+        $admin->roles()->attach($role_admin);
     }
 }
